2bd76f71235320087be26bd8a6576d0295976057
Abdelrahman Magdi <A.Magdi2190@nu.edu.eg>
2025-01-16 18:44:04 +0200

    sign-in page design1

--- client/src/app/sign-in/[[...sign-in]]/page.tsx
+++ client/src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import { SignIn } from "@clerk/nextjs";
 
-export default function page() {
+export default function Page() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <SignIn routing="hash" />
+    <div className="flex h-screen">
+      <div className="flex-1 bg-gray-100 flex items-center justify-center p-3">
+        <div className="w-full h-full bg-blue-500 rounded-[40px] flex items-center justify-center">
+          <h1 className="text-white text-2xl font-bold">Logo</h1>
+        </div>
+      </div>
+      <div className="flex-1 flex items-center justify-center bg-white">
+        <SignIn routing="hash" />
+      </div>
     </div>
   );
 }
