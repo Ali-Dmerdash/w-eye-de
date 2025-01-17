508bbfb868b2a2372e4fd819516903d5c87e3075
Abdelrahman Magdi <A.Magdi2190@nu.edu.eg>
2025-01-17 21:00:54 +0200

    graph component added

---
+++ client/src/app/fraud-page/[[...fraud-page]]/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Graph from "@/app/fraud-page/[[...fraud-page]]/components/graph";
+export default function page() {
+  return (
+    <div>
+      <Graph />
+    </div>
+  );
+}
--- client/src/data/data.ts
+++ client/src/data/data.ts
@@ -17,7 +17,7 @@
         cols: cause, recommendation
 
     Bichart of fraud risk score distribution
-        cols: date, score (percentage) => transaction is divided on three categories (risk, medium risk, no risk) the bi chart represents percentage of each category of each transaction
+        cols: score (percentage) => transaction is divided on three categories (risk, medium risk, no risk) the bi chart represents percentage of each category of each transaction
 
 */
 const clientData = [
@@ -60,6 +60,12 @@ const clientData = [
         { type: "Card Skimming", percentage: 0.2 },
       ],
     },
+    AmmenReport: {
+      // AmmenReport is a report that is concerned about the whole pattern not just the transactino itself
+      cause: "High-value transaction flagged",
+      recommendation:
+        "Require additional authentication for transactions above $1,000.",
+    },
     aiRecommendations: [
       {
         cause: "Unusual transaction location",
@@ -72,65 +78,65 @@ const clientData = [
       },
     ],
     fraudRiskScoreBiChartData: [
-      { date: "2025-01-13", risk: 25, mediumRisk: 40, noRisk: 35 },
-      { date: "2025-01-15", risk: 20, mediumRisk: 25, noRisk: 55 },
+      { risk: 25, mediumRisk: 40, noRisk: 35 },
+      { risk: 20, mediumRisk: 25, noRisk: 55 },
     ],
   },
-  {
-    clientId: 102,
-    transactionData: [
-      {
-        id: 3,
-        name: "Jane Smith",
-        date: "2025-01-14",
-        description: "grouped Payment",
-        amount: 45.8,
-        category: "Salaries",
-        type: "Debit",
-        currency: "USD",
-        fraudRate: 0.05,
-        flag: "Green",
-      },
-      {
-        id: 4,
-        name: "Jane Smith",
-        date: "2025-01-12",
-        description: "Utility Bill",
-        amount: 89.9,
-        category: "Utilities",
-        type: "Debit",
-        currency: "USD",
-        fraudRate: 0.03,
-        flag: "Green",
-      },
-    ],
-    fraudRateGraphData: [
-      { date: "2025-01-12", fraudRate: 0.03 },
-      { date: "2025-01-14", fraudRate: 0.05 },
-    ],
-    fraudIncidentRateData: {
-      mainPercentage: 0.05,
-      commonFraudulentPatterns: [
-        { type: "Account Takeover", percentage: 0.15 },
-        { type: "Identity Theft", percentage: 0.3 },
-      ],
-    },
-    aiRecommendations: [
-      {
-        cause: "High-value transaction flagged",
-        recommendation:
-          "Require additional authentication for transactions above $1,000.",
-      },
-      {
-        cause: "Repeated small transactions",
-        recommendation: "Monitor for potential card testing fraud.",
-      },
-    ],
-    fraudRiskScoreBiChartData: [
-      { date: "2025-01-12", risk: 15, mediumRisk: 35, noRisk: 50 },
-      { date: "2025-01-14", risk: 10, mediumRisk: 30, noRisk: 60 },
-    ],
-  },
+  //   {
+  //     clientId: 102,
+  //     transactionData: [
+  //       {
+  //         id: 3,
+  //         name: "Jane Smith",
+  //         date: "2025-01-14",
+  //         description: "grouped Payment",
+  //         amount: 45.8,
+  //         category: "Salaries",
+  //         type: "Debit",
+  //         currency: "USD",
+  //         fraudRate: 0.05,
+  //         flag: "Green",
+  //       },
+  //       {
+  //         id: 4,
+  //         name: "Jane Smith",
+  //         date: "2025-01-12",
+  //         description: "Utility Bill",
+  //         amount: 89.9,
+  //         category: "Utilities",
+  //         type: "Debit",
+  //         currency: "USD",
+  //         fraudRate: 0.03,
+  //         flag: "Green",
+  //       },
+  //     ],
+  //     fraudRateGraphData: [
+  //       { date: "2025-01-12", fraudRate: 0.03 },
+  //       { date: "2025-01-14", fraudRate: 0.05 },
+  //     ],
+  //     fraudIncidentRateData: {
+  //       mainPercentage: 0.05,
+  //       commonFraudulentPatterns: [
+  //         { type: "Account Takeover", percentage: 0.15 },
+  //         { type: "Identity Theft", percentage: 0.3 },
+  //       ],
+  //     },
+  //     aiRecommendations: [
+  //       {
+  //         cause: "High-value transaction flagged",
+  //         recommendation:
+  //           "Require additional authentication for transactions above $1,000.",
+  //       },
+  //       {
+  //         cause: "Repeated small transactions",
+  //         recommendation: "Monitor for potential card testing fraud.",
+  //       },
+  //     ],
+  //     fraudRiskScoreBiChartData: [
+  //       { date: "2025-01-12", risk: 15, mediumRisk: 35, noRisk: 50 },
+  //       { date: "2025-01-14", risk: 10, mediumRisk: 30, noRisk: 60 },
+  //     ],
+  //   },
 ];
 
 module.exports = {
