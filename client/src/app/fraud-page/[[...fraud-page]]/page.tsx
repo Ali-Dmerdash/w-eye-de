6f00a2098cb8e63f3eacaeba218c8de497e5df5f
Abdelrahman Magdi <A.Magdi2190@nu.edu.eg>
2025-01-19 21:49:35 +0200

    added BiChart and FraudInc components also edited the grid

--- client/src/app/fraud-page/[[...fraud-page]]/page.tsx
+++ client/src/app/fraud-page/[[...fraud-page]]/page.tsx
@@ -1,10 +1,22 @@
 import React from "react";
 import TableTransaction from "@/app/fraud-page/[[...fraud-page]]/components/tableTransaction";
+import Graph from "@/app/fraud-page/[[...fraud-page]]/components/graph";
+import FraudInc from "@/app/fraud-page/[[...fraud-page]]/components/FraudInc";
 
-export default function page() {
+export default function Page() {
   return (
-    <div>
-      <TableTransaction />
+    <div className="grid grid-cols-3 grid-rows-2 gap-4 w-full h-full p-4 bg-secondary ">
+      <div className="col-span-2 row-span-1">
+        <Graph />
+      </div>
+
+      <div className="col-span-1 row-span-2">
+        <FraudInc />
+      </div>
+
+      <div className="col-span-2 row-span-1">
+        <TableTransaction />
+      </div>
     </div>
   );
 }
