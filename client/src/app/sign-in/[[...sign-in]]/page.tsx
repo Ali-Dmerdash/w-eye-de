3cab7686299ba812f841e3a01733af81ebb91457
Ali-Dmerdash <alidmerdash@gmail.com>
2025-01-20 15:49:06 +0200

    Revert "feat:sign-in page auth and install @clerk/elements"
    This reverts commit 6419f2ce511b3b631bb6c7f3b5af8992155390b1.

--- client/src/app/sign-in/[[...sign-in]]/page.tsx
+++ client/src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,41 +1,17 @@
-import { Metadata } from "next";
-import Image from "next/image";
-import Link from "next/link";
+import React from "react";
+import { SignIn } from "@clerk/nextjs";
 
-import { cn } from "@/lib/utils";
-import { buttonVariants } from "@/components/ui/button";
-import { UserAuthForm } from "./components/user-auth-form";
-import logo from "../../../assets/LOGO1.png";
-
-export const metadata: Metadata = {
-  title: "Authentication",
-  description: "Authentication forms built using the components.",
-};
-
-export default function AuthenticationPage() {
+export default function Page() {
   return (
-    <>
-      <div className="container   bg-[#000000]  relative h-[100vh] flex flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
-        <div className="relative bg-[#080808] hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
-          <div className="absolute z-20 mt-auto left-0 bottom-0">
-            <Image src={logo} alt="Logo" height={100} />
-          </div>
-        </div>
-        <div className="relative flex flex-col justify-center items-center">
-          <Image src={logo} alt="Logo" height={100} />
-          <p className="text-lg text-muted-foreground">Your Vision, Your Eye</p>
-          <UserAuthForm />
-          <Link
-            href="/examples/authentication"
-            className={cn(
-              buttonVariants({ variant: "ghost" }),
-              " bg-slate-100 mt-4 w-1/2"
-            )}
-          >
-            Login
-          </Link>
+    <div className="flex h-screen">
+      <div className="flex-1 bg-gray-100 flex items-center justify-center p-3">
+        <div className="w-full h-full bg-blue-500 rounded-[40px] flex items-center justify-center">
+          <h1 className="text-white text-2xl font-bold">Logo</h1>
         </div>
       </div>
-    </>
+      <div className="flex-1 flex items-center justify-center bg-white">
+        <SignIn routing="hash" />
+      </div>
+    </div>
   );
 }
