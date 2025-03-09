3b2ac3dafcc68e2d8e4b72080e4182bf3d27e1e1
julianader <julianader4@gmail.com>
2025-03-09 11:45:17 +0200

    Connecting the database

--- server/index.js
+++ server/index.js
@@ -1,12 +1,28 @@
 require("dotenv").config();
 const express = require("express");
+const connectDB = require("./src/config/database");
+const errorHandler = require("./src/utils/errorHandler");
+const authRoutes = require("./src/routes/authRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Connect to database
+connectDB();
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.use("/api/auth", authRoutes);
+
+// Default route
 app.get("/", (req, res) => {
   res.send("Server is running");
 });
+
+// Error handling middleware
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
