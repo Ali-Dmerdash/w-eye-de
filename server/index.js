6cf983aa73ddc133b0d47419ccf91bd5afece6a4
julianader <julianader4@gmail.com>
2025-03-23 16:25:31 +0200

    DatabaseConnection

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
+
 const errorHandler = require("./src/utils/errorHandler");
 const authRoutes = require("./src/routes/authRoutes");
 const fraudRoutes = require("./src/routes/fraudRoutes");
@@ -28,6 +30,17 @@ app.get("/", (req, res) => {
 // Error handling middleware
 app.use(errorHandler);
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+// MongoDB Connection and Server Start
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => {
+    console.log("MongoDB connected");
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+})
+.catch((err) => {
+    console.error("MongoDB connection error:", err);
 });
